69e9264d122954cdafc78c8e599387d6224c9b60
caozheng0401 <1546376420@qq.com>
2021-05-31 18:37:19 +0800

    aa

---
+++ src/stack/_203移除链表元素.java
@@ -0,0 +1,56 @@
+/**
+ * @author cz
+ * @create 2021/5/24 22:21
+ */
+
+/**
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,6,3,4,5,6], val = 6
+ * 输出：[1,2,3,4,5]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-linked-list-elements
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _203移除链表元素 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(-1,head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while(cur != null){
+            if (cur.val == val){
+                pre.next = cur.next;
+            }else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next
+    }
+
+public ListNode test1(ListNode head, int val){
+        if (head == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(-1,head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while(head != null){
+            if (cur.val == val){
+                pre.next = cur.next;
+            }else{
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+}
+
+}
